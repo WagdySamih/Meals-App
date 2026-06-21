c0569fec745c5ab9b6a62c8bfe638ae7307488ab
Wagdy Samih <WagdySamih787@gmail.com>
2026-06-21 12:51:48 +0300

    feat: adds basic navigation

--- App.tsx
+++ App.tsx
@@ -2,13 +2,24 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Categories } from "./src/screens";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import {
+  createStaticNavigation,
+  NavigationContainer,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <SafeAreaProvider style={styles.container}>
         <SafeAreaView style={styles.container}>
-          <Categories />
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="Categories" component={Categories} />
+            </Stack.Navigator>
+          </NavigationContainer>
         </SafeAreaView>
       </SafeAreaProvider>
       <StatusBar style="auto" />
